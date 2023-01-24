9469df23243188abf87b70d0ef91f5e259cc4ba3
Abdelrahman Tlayjeh <abdelrahmantlayjeh@gmail.com>
2023-01-24 12:28:53 +0200

    fix some styles and build article_details page with redirection to it

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, category
+from .views import home, category, article
 
 urlpatterns = [
     path("home/", home),
     path("category/<str:category>/", category),
+    path("article/<int:article_id>/", article),
 ]
