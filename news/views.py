de3d29ea6458f4fc29ccc1493844c9659960af6d
Rachad <rachadaccoumeh@gmailcom>
2023-01-31 19:43:13 +0200

    replace news_provider__is_active with news_source__is_active

--- news/views.py
+++ news/views.py
@@ -91,14 +91,14 @@ def _news_to_json(news) -> str:
 def home(request):
     common_vars = _common_vars(request.user.is_anonymous)
     # top news (for main slider)
-    top_news = News.objects.all().filter(is_archived=False, news_author__is_active=True, news_provider__is_active=True).order_by("-publish_date")[:10]
+    top_news = News.objects.all().filter(is_archived=False, news_author__is_active=True, news_source__is_active=True).order_by("-publish_date")[:10]
     # top news in each selected category
     top_categories_news = {
         category: News.objects.filter(is_top_in_category=True, news_category=category, is_archived=False, news_author__is_active=True,
-                                      news_provider__is_active=True)
+                                      news_source__is_active=True)
         for category in common_vars["selected_categories"]
     }
-    popular_news = News.objects.all().filter(is_archived=False, news_author__is_active=True, news_provider__is_active=True).order_by("-publish_date")[
+    popular_news = News.objects.all().filter(is_archived=False, news_author__is_active=True, news_source__is_active=True).order_by("-publish_date")[
                    :10]
     if not request.user.is_anonymous:
         popular_news = _add_read_later_like_to_news(popular_news, request.user)
@@ -116,7 +116,7 @@ def home(request):
 
 def category(request, category: str):
     category_news = News.objects.filter(news_category=Category.objects.get(name=category), is_archived=False, news_author__is_active=True,
-                                        news_provider__is_active=True)[:20]
+                                        news_source__is_active=True)[:20]
     if not request.user.is_anonymous:
         category_news = _add_read_later_like_to_news(category_news, request.user)
     return render(
@@ -132,7 +132,7 @@ def category(request, category: str):
 
 
 def article(request, article_id: int):
-    articles = News.objects.get(id=article_id, is_archived=False, news_author__is_active=True, news_provider__is_active=True)
+    articles = News.objects.get(id=article_id, is_archived=False, news_author__is_active=True, news_source__is_active=True)
     return render(
         request,
         "article_details.html",
@@ -147,7 +147,7 @@ def article(request, article_id: int):
 @authenticated_required
 def read_later(request):
     read_later = ReadLater.objects.filter(user=request.user, is_removed=False, news__is_archived=False, news__news_author__is_active=True,
-                                          news__news_provider__is_active=True).values_list("news_id", flat=True)
+                                          news__news_source__is_active=True).values_list("news_id", flat=True)
     read_later_news = News.objects.filter(id__in=read_later)
     for article in read_later_news:
         article.readLater = True
@@ -165,7 +165,7 @@ def read_later(request):
 @authenticated_required
 def history(request):
     history = History.objects.filter(user=request.user, is_removed=False, news__is_archived=False, news__news_author__is_active=True,
-                                     news__news_provider__is_active=True).values_list("news_id", flat=True)
+                                     news__news_source__is_active=True).values_list("news_id", flat=True)
     history_news = News.objects.filter(id__in=history)
     history_news = _add_read_later_like_to_news(history_news, request.user)
     return render(
@@ -181,7 +181,7 @@ def history(request):
 
 @authenticated_required
 def favorite(request):
-    favorite = Like.objects.filter(user=request.user, is_removed=False, news__is_archived=False, news__news_provider__is_active=True,
+    favorite = Like.objects.filter(user=request.user, is_removed=False, news__is_archived=False, news__news_source__is_active=True,
                                    news__news_author__is_active=True).values_list("news_id", flat=True)
     favorite_news = News.objects.filter(id__in=favorite)
     favorite_news = _add_read_later_like_to_news(favorite_news, request.user)
