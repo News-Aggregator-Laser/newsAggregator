75caefb5899b05c14fed74cf1845f465add0350d
Rachad <rachadaccoumeh@gmailcom>
2023-02-02 14:14:46 +0200

    author now is clickable and has its own page

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, category, article, read_later, history, your_feed, favorite, registration
+from .views import home, category, article, read_later, history, your_feed, favorite, registration, author
 
 urlpatterns = [
     path("", home),
     path("category/<str:category>/", category),
     path("article/<int:article_id>/", article),
+    path("author/<str:author>/", author),
     path("read_later/", read_later),
     path("history_list/", history),
     path("your_feed/", your_feed),
