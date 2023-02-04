6d6f4307230403e386512fd07abdc74a7d887d81
Abdelrahman Tlayjeh <abdelrahmantlayjeh@gmail.com>
2023-02-05 01:12:23 +0200

    implement tags filtration and organize helpers methods and re-generate tags for all existing news

--- ml_logic/tags_extractor.py
+++ ml_logic/tags_extractor.py
@@ -4,6 +4,35 @@ from time import time
 
 nlp = spacy.load("en_core_web_md")
 
+UNWANTED_LABELS = [
+    "DATE",
+    "MONEY",
+    "CARDINAL",
+    "PERCENT",
+    "ORDINAL",
+    "QUANTITY",
+    "TIME",
+    "ORG",
+]
+
 
 def extract_tags(text: str) -> set:
-    return set(nlp(text).ents)
+    results = [
+        e.text.lower() for e in nlp(text).ents if e.label_ not in UNWANTED_LABELS
+    ]
+    return set(results)
+
+
+distinct_labels = []
+
+
+def _show_distinct_labels(text: str):
+    for e in nlp(text).ents:
+        if e.label_ not in distinct_labels:
+            distinct_labels.append(e.label_)
+            print(f"'{e.text}': {e.label_}")
+
+
+def _show_all_labels(text: str):
+    for e in nlp(text).ents:
+        print(f"'{e.text}': {e.label_}")
